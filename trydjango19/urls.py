c765b62e840ed52d3b7720461657cbca56af51c1
Harsh <harshavardhana.619@gmail.com>
2017-07-05 16:35:32 +0530

    Changed url patterns

--- trydjango19/urls.py
+++ trydjango19/urls.py
@@ -21,6 +21,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^posts/', include("posts.urls", namespace='posts')),
+    url(r'^', include("posts.urls", namespace='posts')),
 ]
 
