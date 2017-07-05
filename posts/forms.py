77f9e5fdd60b3d32d5a807da679b551f0551d695
Harsh <harshavardhana.619@gmail.com>
2017-07-05 14:50:50 +0530

    removed image support

--- posts/forms.py
+++ posts/forms.py
@@ -7,7 +7,6 @@ class PostForm(forms.ModelForm):
 		model = Post
 		fields = [
 			"title",
-			"image",
 			"content",
 			"draft",
 			"publish",
--- posts/models.py
+++ posts/models.py
@@ -21,13 +21,6 @@ class Post(models.Model):
 	user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
 	title = models.CharField(max_length=120)
 	slug = models.SlugField(unique=True)
-	image = models.ImageField(
-				upload_to=upload_location, 
-				null=True, 
-				blank=True, 
-				width_field="width_field", 
-				height_field="height_field"
-			)
 	height_field = models.IntegerField(default=0)
 	width_field = models.IntegerField(default=0)
 	content = models.TextField()
