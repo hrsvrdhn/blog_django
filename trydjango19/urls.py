c80db2b6e07955435ea4dcea565a80541c5c4386
Harsh <harshavardhana.619@gmail.com>
2017-07-05 14:54:15 +0530

    removed static files

--- trydjango19/urls.py
+++ trydjango19/urls.py
@@ -24,6 +24,3 @@ urlpatterns = [
     url(r'^posts/', include("posts.urls", namespace='posts')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
