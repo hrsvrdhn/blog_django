46085328854c760462b10128f85ae08dd04bef8a
Harsh <harshavardhana.619@gmail.com>
2017-07-06 13:57:48 +0530

    removed database

--- trydjango19/settings/base.py
+++ trydjango19/settings/base.py
@@ -83,10 +83,10 @@ DATABASES = {
     }
 }
 
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-# DATABASES['default']['CONN_MAX_AGE'] = 500
+# import dj_database_url
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
+# # DATABASES['default']['CONN_MAX_AGE'] = 500
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
