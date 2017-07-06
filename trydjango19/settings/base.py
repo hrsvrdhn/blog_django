b03312e13d1a4423ae10563e22bc307fe10a015b
Harsh <harshavardhana.619@gmail.com>
2017-07-06 18:50:43 +0530

    sdsadd

--- trydjango19/settings/base.py
+++ trydjango19/settings/base.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
 
 
 CORS_REPLACE_HTTPS_REFERER      = False
