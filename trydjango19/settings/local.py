9c7a9529f7f17d4ccdc0ffd45a9fd3ec000814cd
Harsh <harshavardhana.619@gmail.com>
2017-07-06 13:45:47 +0530

    added database in local and base

--- trydjango19/settings/local.py
+++ trydjango19/settings/local.py
@@ -82,7 +82,10 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+# DATABASES['default']['CONN_MAX_AGE'] = 500
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
