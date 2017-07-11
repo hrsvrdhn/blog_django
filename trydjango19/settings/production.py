4e5e04d9c1bbf447e87c634be48af24c4f30f49d
Harsh <harshavardhana.619@gmail.com>
2017-07-11 12:04:20 +0530

    settings

--- trydjango19/settings/production.py
+++ trydjango19/settings/production.py
@@ -127,7 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "app/static"),
+    os.path.join(BASE_DIR, "staticfiles"),
     # '/var/www/static/',
 ]
 
