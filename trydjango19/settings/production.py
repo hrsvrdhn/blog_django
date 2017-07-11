cfe5dc70ca782dd00a18e52c2dc30c4ddeb589c4
Harsh <harshavardhana.619@gmail.com>
2017-07-11 12:09:32 +0530

    settings

--- trydjango19/settings/production.py
+++ trydjango19/settings/production.py
@@ -123,16 +123,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "staticfiles"),
-    # '/var/www/static/',
-]
-
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
-
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 CORS_REPLACE_HTTPS_REFERER      = True
 HOST_SCHEME                     = "https://"
