c7455e0b442a5a32eec9e7b148e345919856e5a0
Harsh <harshavardhana.619@gmail.com>
2017-07-11 11:46:34 +0530

    changed url.py

--- trydjango19/urls.py
+++ trydjango19/urls.py
@@ -24,5 +24,4 @@ urlpatterns = [
     url(r'^', include("posts.urls", namespace='posts')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  
