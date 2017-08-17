821231746baeb66ff99d946b88eb5ff81f06a6cf
Harsh <harshavardhana.619@gmail.com>
2017-08-18 01:00:04 +0530

    debug again false in production mode

--- trydjango19/settings/production.py
+++ trydjango19/settings/production.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = os.environ.get('SECRET_KEY',"0y@5e90g_^&(pkt@w8i$#w0ihe*6l@s%or96t7bz#huyhdj1va")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['code-beautiful.herokuapp.com','.codebeautiful.tk','.codebeautiful.cf','.codebeautiful.ml']
 
